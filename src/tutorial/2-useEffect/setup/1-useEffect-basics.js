89b18dfa022634da112e886a8cadea96193006de
LKakha <lkakha@gmail.com>
2021-11-11 01:46:33 +0400

    1

--- src/tutorial/2-useEffect/setup/1-useEffect-basics.js
+++ src/tutorial/2-useEffect/setup/1-useEffect-basics.js
@@ -1,9 +1,28 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 // by default runs after every re-render
 // cleanup function
 // second parameter
 const UseEffectBasics = () => {
-  return <h2>useEffect Basics</h2>;
+  const [value, setValue] = useState(0);
+
+  useEffect(() => {
+    console.log("render effect");
+    document.title = `new message: ${value}`;
+  });
+  console.log("render component");
+  return (
+    <>
+      <h2>{value}</h2>
+      <button
+        className="btn"
+        onClick={() => {
+          setValue(value + 1);
+        }}
+      >
+        increase
+      </button>
+    </>
+  );
 };
 
 export default UseEffectBasics;
--- src/tutorial/3-conditional-rendering/setup/1-multiple-returns.js
+++ src/tutorial/3-conditional-rendering/setup/1-multiple-returns.js
@@ -1,7 +1,60 @@
-import React, { useState, useEffect } from 'react';
-const url = 'https://api.github.com/users/QuincyLarson';
+import React, { useState, useEffect } from "react";
+const url = "https://api.github.com/users/QuincyLarson1";
 const MultipleReturns = () => {
-  return <h2>multiple returns</h2>;
+  const [isLoading, setIsLoading] = useState(false);
+  const [isError, setIsError] = useState(false);
+  const [user, setUser] = useState("default user");
+
+  function delay(d) {
+    return new Promise((resolve) => {
+      setTimeout(() => {
+        resolve(1);
+      }, d);
+    });
+  }
+
+  useEffect(() => {
+    setTimeout(() => {
+      setIsLoading(true);
+      fetch(url)
+        .then(async (resp) => {
+          await delay(3000);
+          if (resp.status >= 200 && resp.status <= 299) return resp.json();
+          throw new Error("user not found");
+        })
+        .then((user) => {
+          setUser(user);
+        })
+        .catch((error) => {
+          setIsError(true);
+        })
+        .finally(() => {
+          setIsLoading(false);
+        });
+    }, 3000);
+  }, []);
+
+  if (isLoading) {
+    return <h4>Loading...</h4>;
+  }
+
+  if (isError) {
+    return <h4>Error</h4>;
+  }
+
+  if (typeof user !== "string") {
+    return (
+      <div>
+        <img src={user.avatar_url} alt={user.login} />
+        <div>
+          <h4>{user.login}</h4>
+          <a href={user.html_url}>profile</a>
+        </div>
+      </div>
+    );
+  }
+
+  return <div>no user here</div>;
 };
 
 export default MultipleReturns;
